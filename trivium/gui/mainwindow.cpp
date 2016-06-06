8dcd43b3728d73fca272ca80909fed3151ed2cde
WerthersEchte <arabali@gmx.de>
2016-06-06 23:54:19 +0200

    some painting...

--- trivium/gui/mainwindow.cpp
+++ trivium/gui/mainwindow.cpp
@@ -3,6 +3,8 @@
 #include <QRegExp>
 #include <QRegExpValidator>
 #include <QVBoxLayout>
+#include <QPixmap>
+#include <QPainter>
 
 #include <sstream>
 #include <iostream>
@@ -74,8 +76,15 @@ MainWindow::MainWindow(QWidget *parent)
         vBit->setMinimumSize(14,40);
         mA.push_back(vBit);
 
-        fA->layout()->addWidget(vBit);
+        if( vI == 68 ){
+            lABit69->setText(mTrivium.mA[vI]?"1":"0");
+            vBit->setStyleSheet("QWidget#lbBit { border-style: solid; border-width: 2; border-color: rgb(255, 0, 0); }");
+        }
+        if( vI == 65 ){
+            vBit->setStyleSheet("QWidget#lbBit { border-style: solid; border-width: 2; border-color: rgb(0, 255, 0); }");
+        }
 
+        fA->layout()->addWidget(vBit);
     }
     for( int vI = 0; vI < mTrivium.mB.size(); ++vI ){
         vBit = new Bit( mTrivium.mB[vI], vI+1 );
@@ -84,7 +93,6 @@ MainWindow::MainWindow(QWidget *parent)
         mB.push_back(vBit);
 
         fB->layout()->addWidget(vBit);
-
     }
     for( int vI = 0; vI < mTrivium.mC.size(); ++vI ){
         vBit = new Bit( mTrivium.mC[vI], vI+1 );
@@ -93,8 +101,54 @@ MainWindow::MainWindow(QWidget *parent)
         mC.push_back(vBit);
 
         fC->layout()->addWidget(vBit);
-
     }
+
+    lCBit66_111_110_109->setText((mTrivium.mC[65] != mTrivium.mC[110]) != (mTrivium.mC[109]&&mTrivium.mC[108])?"1":"0");
+
+    QPixmap *pixmap = new QPixmap(1316,40);
+    pixmap->fill(Qt::transparent);
+    QPainter p(pixmap);
+    p.setPen(QPen(QBrush(QColor(Qt::red)),3));
+    p.drawLine(973,40,973,25);
+    p.drawLine(0,25,973,25);
+    p.setPen(QPen(QBrush(QColor(Qt::green)),3));
+    p.drawLine(931,40,931,15);
+    p.drawLine(931,15,1330,15);
+    p.end();
+    lAMTop->setPixmap(*pixmap);
+
+    pixmap = new QPixmap(70,140);
+    pixmap->fill(Qt::transparent);
+    p.begin(pixmap);
+    p.setPen(QPen(QBrush(QColor(Qt::red)),3));
+    p.drawLine(44,24,70,24);
+    p.drawLine(37,35,37,47);
+    p.setPen(QPen(QBrush(QColor(Qt::red)),1));
+    p.drawLine(34,40,38,50);
+    p.drawLine(42,40,38,50);
+    p.drawLine(35,40,38,50);
+    p.drawLine(41,40,38,50);
+    p.drawLine(36,40,38,50);
+    p.drawLine(40,40,38,50);
+
+    p.setPen(QPen(QBrush(QColor(0,0,255)),3));
+    p.drawEllipse(28,51,21,21);
+    p.setPen(QPen(QBrush(QColor(0,0,255)),1));
+    p.drawLine(37,51,37,70);
+    p.drawLine(38,51,38,70);
+    p.drawLine(39,51,39,70);
+    p.drawLine(28,60,65,60);
+    p.drawLine(28,61,67,61);
+    p.drawLine(28,62,65,62);
+
+    p.drawLine(57,57,67,61);
+    p.drawLine(57,58,67,61);
+    p.drawLine(57,59,67,61);
+    p.drawLine(57,63,67,61);
+    p.drawLine(57,64,67,61);
+    p.drawLine(57,65,67,61);
+    p.end();
+    lAFront->setPixmap(*pixmap);
 
 };
 
@@ -185,6 +239,9 @@ void MainWindow::updateRegister(){
 
     for( int vI = 0; vI < mTrivium.mA.size(); ++vI ){
         mA[vI]->setBit(mTrivium.mA[vI]);
+        if( vI == 68 ){
+            lABit69->setText(mTrivium.mA[vI]?"1":"0");
+        }
     }
     for( int vI = 0; vI < mTrivium.mB.size(); ++vI ){
         mB[vI]->setBit(mTrivium.mB[vI]);
@@ -192,6 +249,8 @@ void MainWindow::updateRegister(){
     for( int vI = 0; vI < mTrivium.mC.size(); ++vI ){
         mC[vI]->setBit(mTrivium.mC[vI]);
     }
+
+    lCBit66_111_110_109->setText((mTrivium.mC[65] != mTrivium.mC[110]) != (mTrivium.mC[109]&&mTrivium.mC[108])?"1":"0");
 
 };
 }
